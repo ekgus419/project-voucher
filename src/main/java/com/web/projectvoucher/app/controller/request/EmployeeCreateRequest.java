088547b5ab23f602f1a919c69da6a05bdcec6378
ekgus419 <ekgus419@gmail.com>
2024-07-03 17:50:20 +0900

    [프로젝트 세팅] employee api 구현 : 사원 생성, 조회 api 구현

---
+++ src/main/java/com/web/projectvoucher/app/controller/request/EmployeeCreateRequest.java
@@ -0,0 +1,4 @@
+package com.web.projectvoucher.app.controller.request;
+
+public record EmployeeCreateRequest(String name, String position, String department) {
+}
---
+++ src/main/java/com/web/projectvoucher/app/controller/response/EmployeeResponse.java
@@ -0,0 +1,4 @@
+package com.web.projectvoucher.app.controller.response;
+
+public record EmployeeResponse (Long no, String name, String position, String department) {
+}
